04d44664d7b37a0d9003f9d9755a1594ac921853
ateethBhattarai <ateethbhattarai02@gmail.com>
2023-12-05 12:29:08 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-2xl font-semibold font-serif text-amber-500">
+      Hello World!!
+    </div>
+  );
+};
+
+export default App;
