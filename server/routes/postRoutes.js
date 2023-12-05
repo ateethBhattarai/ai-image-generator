a0335d38059a9a662b786e92d33e598b7a04e5b7
ateethBhattarai <ateethbhattarai02@gmail.com>
2023-12-05 15:55:57 +0545

    db connection and model creation in backend

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { v2 as cloudinary } from "cloudinary";
+
+import Post from "../models/post";
+
+const router = express.Router();
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./mongodb/connectdb.js";
+import postRoutes from "./routes/postRoutes.js";
+import homeRoute from "./routes/homeRoute.js";
 
 dotenv.config();
 
@@ -9,10 +12,19 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/home", homeRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello from backend!!");
 });
 
-app.listen(8080, () => {
-  console.log("server started at: http://localhost:8080");
-});
+try {
+  connectDB(process.env.MONGODB_URL);
+
+  app.listen(8080, () => {
+    console.log("server started at: http://localhost:8080");
+  });
+} catch (error) {
+  console.log(error);
+}
